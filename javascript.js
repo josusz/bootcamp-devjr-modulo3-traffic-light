4d0c58deac96c9949d34646a5604ede033699b73
josusz <josuhcarvalho@gmail.com>
2023-08-26 23:12:26 -0300

    closes #3 - javascript

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function changeTrafficLight(){
+    if(document.getElementById("yellow").className === "light yellow-light"){
+        document.getElementById("yellow").className = "light light-off";
+        document.getElementById("red").className = "light red-light";
+    }
+    else{
+        if(document.getElementById("red").className === "light red-light"){
+            document.getElementById("red").className = "light light-off";
+            document.getElementById("green").className = "light green-light";
+        }
+        else{
+            document.getElementById("green").className = "light light-off";
+            document.getElementById("yellow").className = "light yellow-light";
+        }
+    }
+}
